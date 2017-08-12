de139f6a91abcd44dd24e23c21157f9d792d3ee5
johnzac <zacharia.john92@gmail.com>
2017-08-12 18:30:53 +0000

    Commit after adding functionality to expose ports from the container to the world

--- netnamespace-chroot-bridge/createContainer.sh
+++ netnamespace-chroot-bridge/createContainer.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 source config.sh
+function mapPorts
+{
+if [[ "$1" == "" || "$2" == "" ]]
+then
+echo "SYntax=containerport:hostport eg: 80:8888"
+exit
+fi
+ip netns exec $namespaceName ip route add  default via $bridgeIp
+iptables -t nat -A PREROUTING -p tcp --dport $2 -j DNAT --to-destination $allottedIp:$1
+
+}
 if [ "$1" == "" ]; then
     echo "enter param"
     exit
@@ -24,12 +35,18 @@ ip link set $vethNamespaceName netns $namespaceName
 ip netns exec $namespaceName ip link set dev lo up
 ip netns exec $namespaceName ip addr add "$allottedIp/$netmask" dev $vethNamespaceName
 ip netns exec $namespaceName ip link set  $vethNamespaceName up
+ip netns exec $namespaceName ip route add  default via $bridgeIp
 brctl addif $bridgeName $vethGlobalName
 ip link set  $vethGlobalName up
-nohup unshare  -p -n  -f ip netns exec $namespaceName chroot /home/ubuntu/rootfs '/launchnginx.sh' &
+nohup unshare  -p -n  -f ip netns exec $namespaceName chroot $rootfsDirName '/launchnginx.sh' &
 disown
 echo "Container private ip : $allottedIp"
-
+if [ ! "$2" == "" ]
+then
+containerPort=`echo $2 | grep -oP '.*(?=:)'`
+hostPort=`echo $2 | grep -oP '(?<=:).*'`
+mapPorts $containerPort $hostPort
+fi
 
 
 
